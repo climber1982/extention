2e62e8d6263a5041acfd9e67c1f1ea799c1919f8
climber1982 <403783154@qq.com>
2020-06-03 09:39:02 +0800

    init

--- rabbitmqClient/src/main/java/com/lovo/mq/client/MQClient.java
+++ rabbitmqClient/src/main/java/com/lovo/mq/client/MQClient.java
@@ -1,13 +1,30 @@
 package com.lovo.mq.client;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class MQClient {
 
+//    @RabbitListener(queues = "pointQueueJ174")
+//    public void getMessage(String message){
+//        System.out.println(message);
+//    }
+
     @RabbitListener(queues = "pointQueueJ174")
-    public void getMessage(String message){
+    public void getMessage2(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag){
+
         System.out.println(message);
+        //手动确认
+        try {
+            channel.basicAck(tag,false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- rabbitmqSystem/src/main/java/com/lovo/mq/service/MQServiceImpl.java
+++ rabbitmqSystem/src/main/java/com/lovo/mq/service/MQServiceImpl.java
@@ -1,15 +1,50 @@
 package com.lovo.mq.service;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service(value = "mqService")
 public class MQServiceImpl implements MQService{
     @Autowired
     RabbitTemplate rabbitTemplate;
 
+    //消息确认
+    RabbitTemplate.ConfirmCallback confirmCallback= new  RabbitTemplate.ConfirmCallback(){
+
+        public void confirm(CorrelationData correlationData, boolean b, String s) {
+            System.out.println("correlationData="+correlationData.getId());
+            System.out.println("b="+b);
+            System.out.println("s="+s);
+            //写业务  如果 b=true 代表数据已经放入到队列，根据correlationData 修改本地数据tag=1()
+        }
+    };
+    //回退
+    RabbitTemplate.ReturnCallback returnCallback=new RabbitTemplate.ReturnCallback() {
+        @Override
+        public void returnedMessage(Message message, int i, String s, String s1, String s2) {
+            System.out.println("message="+message);
+            System.out.println("i="+i);
+            System.out.println("s="+s);
+            System.out.println("s1="+s1);
+            System.out.println("s2="+s2);
+        }
+    };
+
     public void seadMessage(String message){
-        rabbitTemplate.convertAndSend("pointDirectExchange","pointKey",message);
+        rabbitTemplate.setMandatory(true);//开启消息确认
+        //绑定回调函数
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.setReturnCallback(returnCallback);
+       // String uuid= UUID.randomUUID().toString();
+        //消息确认唯一标志，一定要保证唯一
+        CorrelationData correlationData=new CorrelationData("order0001");
+       // System.out.println(correlationData.getId()+"/uuid="+uuid);
+        rabbitTemplate.convertAndSend("pointDirectExchange","pointKey",message,correlationData);
+
     }
 }
